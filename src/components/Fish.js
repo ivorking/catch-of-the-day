d763e559c81bc585a950fe95d586abc8fe6d817d
Ivor King <ivorking@gmail.com>
2018-07-04 11:30:17 +0800

    update

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,10 +2,9 @@ import React from "react";
 import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
-
    render() {
       const { image, name, price, desc, status } = this.props.details;
-      const isAvailable = status === 'avalailable';
+      const isAvailable = status === "available";
       return (
          <li className="menu-fish">
             <img src={image} alt={name} />
@@ -14,13 +13,15 @@ class Fish extends React.Component {
                <span className="price">{formatPrice(price)}</span>
             </h3>
             <p>{desc}</p>
-            <button disabled={!isAvailable} onClick={this.props.addToOrder(this.props.index)}>
-               {isAvailable ? 'Add to order' : 'sold out'}
+            <button
+               disabled={!isAvailable}
+               onClick={() => this.props.addToOrder(this.props.index)}
+            >
+               {isAvailable ? "Add To Order" : "Sold Out!"}
             </button>
          </li>
       );
    }
-
 }
 
 export default Fish;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -8,11 +8,10 @@ class StorePicker extends React.Component {
       // 1. Stop the form from submitting
       event.preventDefault();
       // 2. get the text from that input
-      const storeName = this.myInput.value;
+      const storeName = this.myInput.value.value;
       // 3. Change the page to /store/whatever-they-entered
       this.props.history.push(`/store/${storeName}`);
    };
-
    render() {
       return (
          <form className="store-selector" onSubmit={this.goToStore}>
