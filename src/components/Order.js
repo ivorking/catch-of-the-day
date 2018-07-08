a0a6daf40e47d286740338d0a5b89ed136cd58b2
Ivor King <ivorking@gmail.com>
2018-07-08 18:28:56 +0800

    update

--- src/components/Order.js
+++ src/components/Order.js
@@ -8,16 +8,17 @@ class Order extends React.Component {
       const fish = this.props.fishes[key];
       const count = this.props.order[key];
       const isAvailable = fish && fish.status === "available";
+      const transitionOptions = {
+         classNames: "order",
+         key,
+         timeout: { enter: 500, exit: 500 }
+      };
       // Make sure fish loaded befroe continue
       if(!fish) return null;
 
       if (!isAvailable) {
          return (
-            <CSSTransition
-               classNames="order"
-               key={key}
-               timeout={{ enter: 5000, exit: 5000 }}
-            >
+            <CSSTransition {...transitionOptions}>
                <li key={key}>
                   Sorry {fish ? fish.name : "fish"} is no longer available
                </li>
@@ -25,18 +26,14 @@ class Order extends React.Component {
          );
       }
       return (
-         <CSSTransition
-            classNames="order"
-            key={key}
-            timeout={{ enter: 250, exit: 250 }}
-         >
+         <CSSTransition {...transitionOptions}>
             <li key={key}>
                <span>
                   <TransitionGroup component="span" className="count">
                      <CSSTransition
                         classNames="count"
                         key={count}
-                        timeout={{ enter: 5000, exit: 5000 }}
+                        timeout={{ enter: 500, exit: 500 }}
                      >
                         <span>{count}</span>
                      </CSSTransition>
@@ -66,7 +63,7 @@ class Order extends React.Component {
       return (
          <div className="order-wrap">
             <h2>Order</h2>
-            <TransitionGroup component="ul" classsName="order">
+            <TransitionGroup component="ul" className="order">
                {orderIds.map(this.renderOrder)}
             </TransitionGroup>
             <div className="total">
